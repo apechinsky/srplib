471405ac793d8396df1871fbd85a4ad3499b58b2
Anton Pechinsky <anton@pechinsky.com>
2013-02-22 17:01:12 +0300

    Add ObjectGraph test

--- support/src/test/java/org/srplib/objectgraph/ObjectGraphTest.java
+++ support/src/test/java/org/srplib/objectgraph/ObjectGraphTest.java
@@ -1,17 +1,39 @@
 package org.srplib.objectgraph;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.srplib.contract.Assert;
-import org.srplib.objectgraph.ObjectGraph;
+import static org.hamcrest.core.Is.is;
 
 /**
+ * Test for ObjectGraph.
  *
  * @author Anton Pechinsky
  */
 public class ObjectGraphTest {
+
+    private ObjectTracker tracker;
+
+    @Before
+    public void setUp() throws Exception {
+        this.tracker = new ObjectTracker();
+    }
+
+    @Test
+    public void testTraversal() throws Exception {
+        TestObject testObject = new TestObject();
+
+        ObjectTrackerVisitor trackerVisitor = new ObjectTrackerVisitor();
+        new ObjectGraph(testObject).accept(trackerVisitor);
+
+        Assert.assertThat(trackerVisitor.getTracker().objects, is(tracker.objects));
+    }
+
 
     @Test
     public void testCyclicReferences() throws Exception {
@@ -22,7 +44,7 @@ public class ObjectGraphTest {
         person2.friend = person1;
 
         ObjectGraph objectGraph = new ObjectGraph(person1);
-        objectGraph.accept(new TestVisitor());
+        objectGraph.accept(new ObjectTrackerVisitor());
     }
 
 
@@ -32,16 +54,54 @@ public class ObjectGraphTest {
 
     }
 
-    private class TestVisitor implements Visitor {
+    /**
+     * ObjectGraph test object.
+     *
+     * <p>Registers traversable objects for later verification.</p>
+     */
+    private class TestObject {
 
-        private Set visited = new HashSet();
+        private int intValue = 123;
 
+        private String stringValue = "111";
+
+        private String[] stringArray = { "1", "2", "3" };
+
+        private Set<String> set = new HashSet<String>(Arrays.<String>asList("5", "6", "7"));
+
+        private TestObject() {
+            tracker.track(this);
+            tracker.track(stringArray);
+            tracker.track(set);
+        }
+    }
+
+
+    private class ObjectTrackerVisitor implements Visitor {
+
+        private ObjectTracker tracker = new ObjectTracker();
 
         @Override
         public void visit(Object object) {
-            Assert.checkFalse(visited.contains(object), "Object already has been visited [%s]", object);
-            visited.add(object);
+            tracker.track(object);
+        }
 
+        public ObjectTracker getTracker() {
+            return tracker;
+        }
+    }
+
+
+    private class ObjectTracker {
+
+        private Set objects = new HashSet();
+
+        public <T> T track(T value) {
+            org.srplib.contract.Assert.checkFalse(objects.contains(value), "Object already has been tracked [%s]", value);
+
+            objects.add(value);
+
+            return value;
         }
     }
 }
